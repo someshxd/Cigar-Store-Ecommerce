b237dd422ef07fd2e08851add373281a9faca0a1
Somesh <someshverma8055@gmail.com>
2023-09-07 13:16:05 +0530

    useFetch

--- Client/src/hooks/useFetch.js
+++ Client/src/hooks/useFetch.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { fetchDataFromApi } from "../utils/api";
+import { fetchDataFromApi } from "../Utils/api";
 
 const useFetch = (endpoint) => {
   const [data, setData] = useState();
