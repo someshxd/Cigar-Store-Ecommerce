bfddd650b5d2c16f4b30738d42f8ea518d4b57f6
Somesh <someshverma8055@gmail.com>
2023-09-12 11:33:35 +0530

    helpers

--- Client/src/Utils/helpers.js
+++ Client/src/Utils/helpers.js
@@ -2,3 +2,4 @@ export const VITE_DEV_URL = "http://192.168.29.35:1337";
 
 export const VITE_APP_STRIPE_PUBLISHABLE_KEY =
   "pk_test_51NlkAsSG9eQrlsnUpUI3oxIaiX1R4dnCoTyTseLDvAZuzISey2XamvWwfjmm66nTZr2GSfVqKh2ba530rAeGIxXN00ZXpQq5vu";
+//helpers
